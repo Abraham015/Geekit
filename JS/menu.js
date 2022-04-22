59ab4af849198d74e7a10ff6ac7e50299d7392c8
Abraham015 <79383720+Abraham015@users.noreply.github.com>
2022-04-22 18:42:01 -0500

    22-04-2022
    En este último commit viene la pantalla principal del sistema completa, viene con algunas actualizaciones para la versión movil

--- JS/menu.js
+++ JS/menu.js
@@ -18,19 +18,4 @@ function main() {
             });
         }
     });
-    $(".menu_Rec").click(function() {
-        // $('nav').toggle();
-        elemento = document.getElementById("Recomendaciones");
-        if (contador == 1) {
-            $(elemento).animate({
-                left: "0",
-            });
-            contador = 0;
-        } else {
-            contador = 1;
-            $(elemento).animate({
-                left: "-100%",
-            });
-        }
-    });
 }
