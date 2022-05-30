8765fbe144a7f350b76d445e9028c3aefaad3e8c
ReinaLeonel <reina.zirtaeb@gmail.com>
2022-05-29 23:38:01 -0500

    Correccion en CSS y creacion de archivos Blog

---
+++ src/components/Blog.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import '../css/Blog.css';
+
+export default class Blog extends Component {
+    render() {
+      return (
+        <div className="blog">
+            <div className="textoEncima">
+            </div>
+        </div>
+        )
+    }
+}
