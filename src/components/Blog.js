07ec55be3c60ac0b36994e82c8b3f3db524d39cc
AbisH015 <abraham@qualityanalytics.io>
2022-05-31 19:07:17 -0500

    Pantallas de Reina

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -6,7 +6,7 @@ export default class Blog extends Component {
       return (
         <div className="blog">
             <img src={process.env.REACT_APP_BASE_URL_IMAGES + "/BarraBlog.jpg"} className="imagenBlog" alt=""/>
-            <div className="perfil">
+            <div className="perfil columnside">
                 <img src={process.env.REACT_APP_BASE_URL_IMAGES + "/user.jpg"} className="imagenPerfil" alt=""/>
                 <h4 className="nombreUsuario">Nombre usuario</h4>
                 <h4 className="nickname">Nickname</h4>
@@ -14,12 +14,14 @@ export default class Blog extends Component {
                 <p className="txtDes">Algunos datos informativos del usuario (gustos, redes sociales)</p>
                 <p className="buttonP">Configurar Perfil</p>
             </div>
-            <div className="contenido">
-                <section className='crearPublicacion'>
-                    <input className='escribe' type="text" placeholder="Escribe algo..."/>
-                </section>
+            <div className="contenido-columnmiddle">
+                <div className="crearPublicacion">
+                    <input className="escribe" type="text" placeholder="Escribe algo..."/>
+                    <p className="buttonPub">Publicar</p>
+                </div>
             </div>
         </div>
         )
     }
 }
+
--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -23,26 +23,3 @@ export default class Inicio extends Component {
     );
   }
 }
-
-/*function inicio(props) {
-  return (
-    <div className="inicio">
-      <cabecera />
-      <div className="CuadroGrande">
-        <img src={"Images/iniciosesion.png"} className="avatar" alt="fondo" />
-        <h1> Iniciar Sesión </h1>{" "}
-        <form action="">
-          <label for="username"> Usuario </label>{" "}
-          <input type="text" placeholder="Usuario" />
-          <label for="password"> Contraseña </label>{" "}
-          <input type="password" placeholder="Contraseña" />
-          <input type="submit" value="Entrar" />
-          <a href="!#"> ¿Olvidaste tu contraseña ? </a> <br />
-          <a href="!#"> ¿No tienes una cuenta ? </a>{" "}
-        </form>{" "}
-      </div>{" "}
-    </div>
-  );
-}
-
-export default inicio;*/
