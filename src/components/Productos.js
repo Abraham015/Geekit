46fc1f8914de86e57d84e858499f9a6992451ed5
Gloria-2001 <olimen141@gmail.com>
2022-06-03 02:29:15 -0500

    Ajustes y componente Products

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -1,83 +1,37 @@
 import React, { Component} from "react";
 import "../css/Productos.css";
+import Products from "./Products/Products";
 
 export default class Productos extends Component {
   render() {
     return (
       <div className="productos">
-        <div className="row">
-          <div className="column side"> </div>{" "}
-          <div class="column middle">
-            <div class="contendedorBusqueda">
-              <div class="buscar">
+          <br />
+
+          <div className="column middle">
+            <div className="contendedorBusqueda">
+              <div className="buscar">
                 <input type="search" placeholder="Buscar" id="inputSearch" />
                 <button type="submit">
-                  <i className="fas fa-search"> </i>{" "}
-                </button>{" "}
-              </div>{" "}
-              <div class="Filtro">
+                  <i className="fas fa-search"> </i>
+                </button>
+              </div>
+              <div className="Filtro">
                 <p>
-                  <i class="fa-solid fa-sliders"> </i>{" "}
-                </p>{" "}
-              </div>{" "}
-            </div>{" "}
+                  <i className="fa-solid fa-sliders"> </i>
+                </p>
+              </div>
+            </div>
             <br />
             <br />
-          </div>{" "}
-          <div class="column side"> </div>{" "}
-        </div>{" "}
-        <div className="row2">
-          <div className="column middle2">
-            <div className="card">
-              <img
-                src={process.env.REACT_APP_BASE_URL_IMAGES + "/AoTBefore.png"}
-                className="img imagenes"
-                alt=""
-              />
-              <div className="content">
-                <div className="NombreProducto">Ataque de los Titanes - Before The Fall</div>{" "}
-                <div className="">
-                  <div className="Precio">$1,683.00</div> 
-                  <div> Este es un mundo controlado por los titanes... La humanidad, convertida en alimento de gigantes, ha construido paredes gigantescas, y pagó el poder evitar los ataques con su libertad.</div>{" "}
-                </div>{" "}
-              </div>{" "}
-            </div>{" "}
-          </div>{" "}
-          <div className="column middle2">
-            <div className="card">
-              <img
-                src={process.env.REACT_APP_BASE_URL_IMAGES + "/Boys.jpg"}       
-                className="img imagenes"
-                alt=""
-              />
-              <div className="content">
-                <div className="NombreProducto">The Boys Box Set | Serie Completa</div>{" "}
-                <div className="">
-                  <div className="Precio">$3,300.00</div> 
-                  <div>Pocas cosas son tan peligrosas como los "súpers", sobre todo porque se trata de personas torpes, corruptas, viciosas y ególatras, tal como todas las demás del mundo.</div>{" "}
-                </div>{" "}
-              </div>{" "}
-            </div>{" "}
-          </div>{" "}
-          <div className="column middle2">
-            <div className="card">
-              <img
-                src={process.env.REACT_APP_BASE_URL_IMAGES + "/Levi.jpg"}
-                className="img imagenes"
-                alt=""
-              />
-              <div className="content">
-                <div className="NombreProducto">ATTACK ON TITAN THE FINAL SEASON-LEVI</div>{" "}
-                <div className="">
-                  <div className="Precio">$839.00</div> 
-                  <div>Figura coleccionable del Capitán Levi Ackerman de la serie "Attack on Titan"</div>{" "}
-                </div>{" "}
-              </div>{" "}
-            </div>{" "}
-          </div>{" "}
-        </div>{" "}
+          </div>
+          
+          <br /> 
+          <Products productImage={process.env.REACT_APP_BASE_URL_IMAGES + "/Levi.jpg"} productName={"Attack on Titan The Final Season - LEVI"} productPrice={"$839.00"} productDescription={"Figura coleccionable del Capitán Levi Ackerman de la serie 'Attack on Titan'"} productName2={"The Boys Box Set | Serie Completa"} productPrice2={"$3,300.00"} productDescription2={"Pocas cosas son tan peligrosas como los 'súpers', sobre todo porque se trata de personas torpes, corruptas, viciosas y ególatras, tal como todas las demás del mundo."} productImage2={process.env.REACT_APP_BASE_URL_IMAGES + "/Boys.jpg"} productName3={"Ataque de los Titanes - Before The Fall"} productPrice3={"$1,683.00"} productDescription3={"Este es un mundo controlado por los titanes... La humanidad, convertida en alimento de gigantes, ha construido paredes gigantescas, y pagó el poder evitar los ataques con su libertad."} productImage3={process.env.REACT_APP_BASE_URL_IMAGES + "/AoTBefore.png"}/>
 
-        <br />
+          <Products productImage={process.env.REACT_APP_BASE_URL_IMAGES + "/sableLuz.jpg"} productName={"Star Wars Darth Vader - Sable de luz roja"} productPrice={"$762.15"} productDescription={"Sable de luz de Darth Vader rojo, iluminado, con luces, sonidos y frases."} productName2={"X-Men: Age of Apocalypse Omnibus"} productPrice2={"$7,466.32"} productDescription2={"Charles Xavier está muerto, asesinado en el pasado durante un accidente de viaje en el tiempo, Apocalipsis gobierna a través de su credo de la 'supervivencia del más apto' ... pero escondidos entre una humanidad oprimida están los luchadores por la libertad de Magneto: ¡los X-Men!"} productImage2={process.env.REACT_APP_BASE_URL_IMAGES + "/x-menComic.jpg"} productName3={"Funko Pop! Leonard Hofstadter"} productPrice3={"$339.00"} productDescription3={"Figura coleccionable Funko Pop! de Leonard Hofstadter, The Big Bang Theory"} productImage3={process.env.REACT_APP_BASE_URL_IMAGES + "/leonardTBBT.jpg"}/>
+
+          <Products productImage={process.env.REACT_APP_BASE_URL_IMAGES + "/pikachu.jpg"} productName={"Peluche Pikachu"} productPrice={"$976.92"} productDescription={"Peluche de Pikachu con licencia oficial de Sanei Pokemon"} productName2={"Juego de llaveros Marvel"} productPrice2={"$650.00"} productDescription2={"Juego de 6 llaveros para los amantes de Marvel"} productImage2={process.env.REACT_APP_BASE_URL_IMAGES + "/llaverosMarvel.png"} productName3={"Playera Flash, DC Comics"} productPrice3={"$239.00"} productDescription3={"Playera de Flash para Caballero, color rojo, unitalla"} productImage3={process.env.REACT_APP_BASE_URL_IMAGES + "/playeraFlash.jpg"}/>
       </div>
     );
   }
---
+++ src/components/Products/Products.js
@@ -0,0 +1,37 @@
+import React from "react";
+import './products.css';
+
+const Products = ({productName, productImage, productPrice, productDescription, productName2, productImage2, productPrice2, productDescription2, productName3, productImage3, productPrice3, productDescription3}) => {
+  return (
+    <div className="wrapperProductos">
+        <div className="rowProductos">
+            <div className="card">
+                <div className="img imagenes">
+                    <img src={productImage} className="imgProd"/>
+                </div>
+                <p className="NombreProducto">{productName}</p>
+                <p className="Precio">{productPrice}</p>
+                <p className="descripcionProducto">{productDescription}</p>
+            </div>
+            <div className="card">
+                <div className="img imagenes">
+                    <img src={productImage2} className="imgProd"/>
+                </div>
+                <p className="NombreProducto">{productName2}</p>
+                <p className="Precio">{productPrice2}</p>
+                <p className="descripcionProducto">{productDescription2}</p>
+            </div>
+            <div className="card">
+                <div className="img imagenes">
+                    <img src={productImage3} className="imgProd"/>
+                </div>
+                <p className="NombreProducto">{productName3}</p>
+                <p className="Precio">{productPrice3}</p>
+                <p className="descripcionProducto">{productDescription3}</p>
+            </div>
+        </div>
+    </div>
+  );
+};
+
+export default Products;
